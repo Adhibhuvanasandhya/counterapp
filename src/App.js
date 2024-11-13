dd25b03b7052d7d606fb8c80b11da6300f3876fa
Adhibhuvanasandhya <sandhyaponrajan.l@gmail.com>
2024-11-13 13:28:29 +0530

    updated project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import one from "./images/clock.png"
+import { useState } from 'react';
+function Counter() {
+    const [counter, setCounter] = useState(0);
+    const [history, setHistory] = useState([]); 
+  
+    
+    const increase = () => {
+      const newCounter = counter + 1;
+      setCounter(newCounter);
+      setHistory([...history, newCounter]);
+    };
+  
+  
+    const decrease = () => {
+      if (counter === 0) {
+        alert("Counter cannot go below zero!");
+        return;
+      }
+      const newCounter = counter - 1;
+      setCounter(newCounter);
+      setHistory([...history, newCounter]);
+    };
+  
+    
+    const reset = () => {
+      setCounter(0);
+      setHistory([]);
+    };
+  
+    return (
+      <div className="app">
+        <h1>Counter App</h1>
+        <div className="counter-value">
+          <strong>{counter}</strong>
+        </div>
+        <div className="buttons">
+          <button onClick={increase}>+</button>
+          <button onClick={decrease}>-</button>
+          <button className="reset" onClick={reset}>Reset</button>
+        </div>
+  
+      
+        <div className="history">
+          <h2 style={{color:"white"}}><img src={one} alt='his'></img>History</h2>
+          <div className="history-list">
+            {history.length === 0 ? (
+              <p>No history yet.</p>
+            ) : (
+              <ul>
+                {history.map((value, index) => (
+                  <li key={index}> {value}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+export default Counter  
